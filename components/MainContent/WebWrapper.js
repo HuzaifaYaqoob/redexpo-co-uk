5d737ec7d909e5594a7a689fdb250247f3ff60a0
unknown <zeevhyorg@gmail.com>
2022-01-03 00:18:45 +0500

    Fav icon and title added

--- components/MainContent/WebWrapper.js
+++ components/MainContent/WebWrapper.js
@@ -1,10 +1,15 @@
 
 import Header from "../Header/Header"
+import Head from 'next/head'
 
 
 const WebWrapper = (props) => {
     return (
         <>
+            <Head>
+                <title>RedExpo | The Future Is Here</title>
+                <link rel="shortcut icon" href="/images/icon.svg" type="image/x-icon" />
+            </Head>
             <Header />
             {
                 props.children
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-
 import WebWrapper from '../components/MainContent/WebWrapper'
 
 function MyApp({ Component, pageProps }) {
