f0140949dda677564eb4b7b1472da9fb0f91b6c4
unknown <zeevhyorg@gmail.com>
2022-01-04 07:04:34 +0500

    Hero section doing with responsiveness

--- components/HomePage/HeroSection.js
+++ components/HomePage/HeroSection.js
@@ -1,12 +1,25 @@
 
 
+const HeadingSection = (props) => {
+    return (
+        <div className="px-5">
+            <h3 className="text-red-600">Your success partner</h3>
+            <h1 className="text-3xl md:text-left md:text-6xl mb-3 md:mb-9 font-medium text-dark-RE">Business value through digital products.</h1>
+            <p className="mb-9 md:mb-0">Helping <span className="text-red-600">Startups</span> and <span className="text-red-600">Growth Companies</span>. We build
+                custom softwares, providing reliable end-to-end
+                design & development solutions</p>
+        </div>
+    )
+}
+
 
 const HeroSection = () => {
     return (
         <div
-            className={`w-full max-w-6xl px-5 mx-auto bg-red-100 my-20`}
+            className={`w-full max-w-7xl px-5 md:px-0 mx-auto my-20 md:flex md:items-center`}
         >
-            hi
+            <HeadingSection />
+            <img className="w-full max-w-2xl" src="/images/bulb-img.svg" alt="" />
         </div>
     )
 }
