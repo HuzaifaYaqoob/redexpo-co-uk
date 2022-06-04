e4f535a6b30f37fea82e43122effba6534f6bf91
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-06-04 19:23:26 +0500

    form section contact us section mobile ui and form handleing

---
+++ src/Redux/api_constants.js
@@ -0,0 +1,9 @@
+
+
+
+const BASE_URL = 'https://services.redexpo.co.uk'
+export const APIBASE_URL = `${BASE_URL}/api`
+
+export const contact_url = '/contact_us'
+
+export default BASE_URL
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      ...defaultTheme.screens,
+      'md-lg' : '896px',
+    },
   },
   plugins: [],
 }
